55c907bcd98e1e26d01f402a226d4af4aca495c7
elite1003 <sudhirkumar10397@gmail.com>
2024-02-12 18:34:36 +0530

    task 8 variables added

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-
+import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date().toLocaleDateString();
+  const expenseDescription = "Car insurance";
+  const expensePrice = 3000;
+  const locationOfExpenditure = "Lucknow";
   return (
-    <>
-      <div>Expense Items</div>
-
-      <h2>Food Rs 10</h2>
-      <h2>Petrol Rs 100</h2>
-      <h2>Movies Rs 200</h2>
-    </>
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__description">
+        <h2>{expenseDescription}</h2>
+        <h2>{locationOfExpenditure}</h2>
+        <div className="expense-item__price">{expensePrice}</div>
+      </div>
+    </div>
   );
 }
 export default ExpenseItem;
